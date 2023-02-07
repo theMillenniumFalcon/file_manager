00cefbf032733817273aa4ed648647da202b1264
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-02-07 17:08:01 +0530

    added useAppSelector hook

--- screens/Main.tsx
+++ screens/Main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native'
 
+import { useAppSelector } from '../hooks/reduxHooks'
+
 export default function Main() {
+    const { theme } = useAppSelector((state: { theme: any }) => state.theme)
+
     return (
-        <View style={{
-            flex: 1,
-            backgroundColor: 'red'
-        }}>
+        <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
             <Text>Main screen</Text>
         </View>
     )
