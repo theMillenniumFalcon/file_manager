56b715f9b632251ef17f8a64433fabd0e3bc8345
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-02-08 13:15:09 +0530

    added multi image selection hook

---
+++ hooks/useMultiImageSelection.ts
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react"
+import { ExtendedAsset } from "../types/types"
+
+export default function useSelectionChange(items: ExtendedAsset[]) {
+    const [isSelecting, setIsSelecting] = useState(false)
+
+    useEffect(() => {
+        if (items.filter((i) => i.selected).length > 0) {
+            setIsSelecting(true)
+        } else {
+            setIsSelecting(false)
+        }
+    }, [items])
+
+    return isSelecting
+}
