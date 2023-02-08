53bbd298415777b0b53095fbc3242f013cc82d22
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-02-08 22:47:00 +0530

    app now working

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import 'react-native-gesture-handler'
 import React from 'react'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { LogBox } from 'react-native'
 import { Provider } from 'react-redux'
+
 import Main from './screens/Main'
 import { store } from './store'
 
@@ -9,9 +11,11 @@ LogBox.ignoreLogs(['componentWillMount', 'componentWillReceiveProps'])
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Main />
-    </Provider>
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    </SafeAreaProvider>
   )
 }
 
