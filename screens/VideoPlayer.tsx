3366821875e265a6bbf8cac5e41b85d961b574b8
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-02-09 20:59:15 +0530

    added web and videoplayer screens

--- screens/VideoPlayer.tsx
+++ screens/VideoPlayer.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { StyleSheet, Text } from 'react-native'
-import { StackScreenProps } from "@react-navigation/stack"
-import { SafeAreaView } from 'react-native-safe-area-context'
-import Constants from 'expo-constants'
-
+import { StyleSheet } from 'react-native'
+import React, { useRef } from 'react'
+import { StackScreenProps } from '@react-navigation/stack'
 import { useAppSelector } from '../hooks/useAppSelector'
+import { ResizeMode, Video } from 'expo-av'
+import Constants from 'expo-constants'
+import { SIZE } from '../utils/Constants'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 type VideoViewParamList = {
     VideoPlayer: { prevDir: string; folderName: string }
@@ -14,16 +15,30 @@ type Props = StackScreenProps<VideoViewParamList, 'VideoPlayer'>
 
 export default function VideoPlayer({ route }: Props) {
     const { colors } = useAppSelector((state) => state.theme.theme)
+    const { prevDir, folderName } = route.params
+    const videoRef = useRef<Video | null>(null)
 
     return (
         <SafeAreaView
             style={{ ...styles.container, backgroundColor: colors.background }}
         >
-            <Text>Video player</Text>
+            <Video
+                ref={videoRef}
+                style={{
+                    width: SIZE,
+                    height: '100%',
+                }}
+                source={{
+                    uri: prevDir + folderName,
+                }}
+                useNativeControls
+                shouldPlay
+                resizeMode={ResizeMode.COVER}
+                isLooping
+            />
         </SafeAreaView>
     )
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
