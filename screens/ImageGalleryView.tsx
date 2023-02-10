9dc0e7fc1362893810fcb3f5853b3b41e0471129
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-02-10 11:39:00 +0530

    added image gallery view screen

--- screens/ImageGalleryView.tsx
+++ screens/ImageGalleryView.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React, { useCallback, useEffect } from 'react'
+import { StyleSheet, View } from 'react-native'
+import ImageView from 'react-native-image-viewing'
 import { StackScreenProps } from '@react-navigation/stack'
+import { useNavigation } from '@react-navigation/native'
 
 import { useAppSelector } from '../hooks/useAppSelector'
+import { useAppDispatch } from '../hooks/useAppDispatch'
+import { setTabbarVisible } from '../features/files/tabbarStyleSlice'
 
 type FileViewParamList = {
     ImageGalleryView: { prevDir: string; folderName: string }
@@ -11,11 +15,35 @@ type FileViewParamList = {
 type Props = StackScreenProps<FileViewParamList, 'ImageGalleryView'>
 
 const ImageGalleryView = ({ route }: Props) => {
+    const navigation = useNavigation()
+    const dispatch = useAppDispatch()
     const { colors } = useAppSelector((state) => state.theme.theme)
+    const { prevDir, folderName } = route.params
+    const { images } = useAppSelector((state) => state.images)
+
+    const initialImageIndex = useCallback(
+        () => images.findIndex((item) => item.uri === prevDir + folderName), []
+    )
+
+    useEffect(() => {
+        dispatch(setTabbarVisible(false))
+
+        return () => {
+            dispatch(setTabbarVisible(true))
+        }
+    }, [])
 
     return (
         <View style={{ ...styles.container, backgroundColor: colors.background }}>
-            <Text>Image gallery view</Text>
+            <ImageView
+                images={images}
+                imageIndex={initialImageIndex()}
+                visible={true}
+                onRequestClose={() => navigation.goBack()}
+                keyExtractor={(_, index) => index.toString()}
+                doubleTapToZoomEnabled
+                swipeToCloseEnabled
+            />
         </View>
     )
 }
